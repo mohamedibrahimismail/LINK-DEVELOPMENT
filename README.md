484fc09515f3e780e57b0287abe28dfa401085d8
mohamedibrahimismail <mieibrahim15@gmail.com>
2021-05-09 15:00:37 +0200

    adding README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # LINK-DEVELOPMENT
+in this project i used several technologies to achieve best performance , i used :
+
+kotlin
+Android Architecture component
+Mvvm
+RX
+Koin for dependency injection
+Retrofite for calling apis
+Using RXzip operator to handle making multiple calling apis in parallel
+making design compitable with different screens and supporting RTL 
+and considered clean code , easy-to-read code , user-friendly UI and bug-free code.
--- app/src/main/java/com/example/link/viewModels/MainVM.kt
+++ app/src/main/java/com/example/link/viewModels/MainVM.kt
@@ -11,11 +11,6 @@ import io.reactivex.schedulers.Schedulers
 
 class MainVM(private val repository: AppRepository) : BaseViewModel() {
 
-//    var AllObservableArticles: Observable<GenericResponse<MutableList<ArticlesModelItem>>> =
-//        repository.getAssociatedPress()
-//    var TheNextWebObservable: Observable<GenericResponse<MutableList<ArticlesModelItem>>> =
-//        repository.getTheNextWeb()
-
     var AllArticles: MutableLiveData<MutableList<ArticlesModelItem>> = MutableLiveData()
     var ErrorHappened: MutableLiveData<Boolean> = MutableLiveData()
 
@@ -46,87 +41,6 @@ class MainVM(private val repository: AppRepository) : BaseViewModel() {
         )
     }
 
-
-//    var AssociatedPress: MutableLiveData<MutableList<ArticlesModelItem>> = MutableLiveData()
-//    var TheNextWeb: MutableLiveData<MutableList<ArticlesModelItem>> = MutableLiveData()
-//
-//
-//    fun getAssociatedPress() {
-//        loading.value = true
-//        mCompositeDisposable.add(
-//            repository.getAssociatedPress()
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribeWith(object :
-//                    CallbackWrapper<GenericResponse<MutableList<ArticlesModelItem>>>() {
-//                    override fun onSuccess(t: GenericResponse<MutableList<ArticlesModelItem>>) {
-//                        loading.value = false
-//                        AssociatedPress.postValue(t.articles)
-//                    }
-//
-//                    override fun onError(e: Throwable) {
-//                        super.onError(e)
-//                        loading.value = false
-//                    }
-//
-//                    override fun onFail(t: String?) {
-//                        loading.value = false
-//                        //kotlin.error.postValue(t)
-//                    }
-//
-//                    override fun onFail(t: Map<String, ArrayList<String>>) {
-//                        loading.value = false
-////                        t.forEach {
-////                            if (it.key == "email") {
-////                                emailErrors.value = it.value
-////                            } else if (it.key == "password") {
-////                                passwordErrors.value = it.value
-////                            }else if (it.key == "mobile_number") {
-////                                phoneErrors.value = it.value
-////                            }
-////                        }
-//                    }
-//                })
-//        )
-//    }
-//
-//    fun getTheNextWeb() {
-//        loading.value = true
-//        mCompositeDisposable.add(
-//            repository.getTheNextWeb()
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribeWith(object :
-//                    CallbackWrapper<GenericResponse<MutableList<ArticlesModelItem>>>() {
-//                    override fun onSuccess(t: GenericResponse<MutableList<ArticlesModelItem>>) {
-//                        loading.value = false
-//                        TheNextWeb.postValue(t.articles)
-//                    }
-//
-//                    override fun onError(e: Throwable) {
-//                        super.onError(e)
-//                        loading.value = false
-//                    }
-//
-//                    override fun onFail(t: String?) {
-//                        loading.value = false
-//                        //kotlin.error.postValue(t)
-//                    }
-//
-//                    override fun onFail(t: Map<String, ArrayList<String>>) {
-//                        loading.value = false
-////                        t.forEach {
-////                            if (it.key == "email") {
-////                                emailErrors.value = it.value
-////                            } else if (it.key == "password") {
-////                                passwordErrors.value = it.value
-////                            }else if (it.key == "mobile_number") {
-////                                phoneErrors.value = it.value
-////                            }
-////                        }
-//                    }
-//                })
-//        )
 }
 
 
